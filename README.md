b4ef7c82dd57a476ec4b1b03a3d87efc68cde966
Mayeu <m@mayeu.me>
2019-03-06 10:00:12 +0700

    Initial commit :tada:

---
+++ README.md
@@ -0,0 +1,29 @@
+# Omnifocus Level Up
+
+A set of scripts to create tasks and mark them as done automatically in
+Habitica.
+
+## How does this script works
+
+* You check something as done in OmniFocus
+* Magic make the script run
+* The script create a tasks in Habitica and mark it done immediatly
+
+## This script will not
+
+* Sync habitica to OmniFocus
+* Sync undone task to Habitica
+* Sync or track habits and dailies
+
+## Why?
+
+I don't use Habitica for my daylies or todo anymore, but I don't wanted to pass
+on the sweet sweet XP of done tasks.
+
+Thus, this script.
+
+## :warning: WARNING :warning:
+
+This may break your Habitica. It should not break your OmniFocus (it only read stuff), but remember that:
+
+[img/i-have-no-idea-what-im-doing-doing.jpg]
---
+++ done_task_hook.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+readonly DEBUG=false
+
+#-- Dependencies -----------------------------------
+#
+# This script require:
+#  * terminal-notifier(1)
+#  * jq(1)
+#
+# You can install all that via brew:
+#
+#    brew install terminal-notifier jq
+#
+
+function info {
+  terminal-notifier -title "OmniFocus to Habitica" -message "$1"
+}
+
+function debug {
+  if ${DEBUG};
+  then
+    info "$1"
+  fi
+}
+
+debug "Start processing the hooks"
+
+readonly script_dir="${HOME}/Library/Application Support/omnifocus-level-up"
+readonly last_run_file="${script_dir}/last_completed_task_processed"
+readonly last_run_dir="${script_dir}/cache"
+
+# A slug function
+to_slug() {
+  # Forcing the POSIX local so alnum is only 0-9A-Za-z
+  export LANG=POSIX
+  export LC_ALL=POSIX
+  # Keep only alphanumeric value
+  sed -e 's/[^[:alnum:]]/-/g' |
+  # Keep only one dash if there is multiple one consecutively
+  tr -s '-'                   |
+  # Lowercase everything
+  tr A-Z a-z                  |
+  # Remove last dash if there is nothing after
+  sed -e 's/-$//'
+}
+
+# List the todo to process
+todo_to_process() {
+    # TODO: replace the time with something more logical
+    osascript $(dirname $0)/omni-list-done-todo-since.scpt "$last_processed_time" 2>&1 |
+    sed -e 's/^date //'                                                  |
+    sed -e 's/,/|/'
+}
+
+# ensure we have our folder created
+mkdir -p "$script_dir"
+mkdir -p "$last_run_dir"
+# ensure we have the last run file, or create a default one if not
+if ! test -f "${last_run_file}";
+then
+  echo "Thursday 1 January 1970 at 00:00:00" > "$last_run_file" ;
+fi
+
+# Get the last time we ran which is also the last successfully processed tasks completion time
+last_processed_time=`cat "$last_run_file"`
+
+todo_to_process |
+while read -r line;
+do
+  # Split the line in two on the '|'
+  IFS="|" read timestamp title <<< ${line}
+  todo=$(echo "${timestamp}-${title}" | to_slug)
+  cache_file="${last_run_dir}/${todo}"
+
+  if test ! -f "$cache_file"
+  then
+    # Escape the title, but habitica don't like '\ '
+    debug "Process \"${title}\""
+    # todo create the task on habitica
+    json_data=$(habitica create "$todo")
+    debug "Successfully created \"${todo}\""
+
+    task_id=$(jq '.data.id' <<< ${json_data} | sed -e 's/"//g')
+    habitica "done" "$task_id"
+    # todo mark it as done
+    debug "Successfully done \"${title}\""
+    info "Sent to Habitica"
+
+    # Cache the processed todo
+    debug "Caching the todo"
+    touch "$cache_file"
+
+    # bump the last timestamp proccessed
+    echo "$timestamp" > "$last_run_file"
+  fi
+done
+
+debug "Finished completed task processing"
